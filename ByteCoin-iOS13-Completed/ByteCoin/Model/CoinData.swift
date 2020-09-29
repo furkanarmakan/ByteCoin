26a507db4d3528b498ed18edb0d430cfc772a66a
Furkan Armakan <39617002+furkanarmakan@users.noreply.github.com>
2020-09-29 16:57:47 +0300

    Add files via upload

---
+++ ByteCoin-iOS13-Completed/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Furkan Armakan on 25/05/2020.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
